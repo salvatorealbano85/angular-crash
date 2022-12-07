d387662be74fd10ea40d03167f4c120a121572e2
salvatorealbano85 <salvatore.albano@ednlan.local>
2022-12-07 11:49:54 +0100

    Aggiunto Component Tasks

---
+++ src/app/components/tasks/tasks.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { TASKS } from 'src/app/mock-tasks';
+
+@Component({
+  selector: 'app-tasks',
+  templateUrl: './tasks.component.html',
+  styleUrls: ['./tasks.component.css']
+})
+export class TasksComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
