81119eaca4cf3ce3fe2dcfdc710abededa77b430
salvatorealbano85 <salvatore.albano@ednlan.local>
2022-12-07 12:52:11 +0100

    Aggiunto Taskservice e observable

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Task } from 'src/app/Task';
-import { TASKS } from 'src/app/mock-tasks';
+import { TaskService } from 'src/app/services/task.service';
+
 
 @Component({
   selector: 'app-tasks',
@@ -8,11 +9,12 @@ import { TASKS } from 'src/app/mock-tasks';
   styleUrls: ['./tasks.component.css']
 })
 export class TasksComponent implements OnInit {
-  tasks: Task[] = TASKS;
+  tasks: Task[] = [];
 
-  constructor() { }
+  constructor(private taskService: TaskService) { }
 
   ngOnInit(): void {
+    this.taskService.getTasks().subscribe((tasks) => this.tasks = tasks);
   }
 
 }
